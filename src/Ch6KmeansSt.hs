74df5d83ffa0a5757ed9f20ade8941019d8dc34b
Mathew Fournier <mat@matfournier.com>
2019-10-11 20:31:47 -0700

    finishes chapter 6

--- src/Ch6KmeansSt.hs
+++ src/Ch6KmeansSt.hs
@@ -5,7 +5,11 @@
 module Ch6KmeansSt where
 
 
+-- exercise 6-7 K-MEANS USING ST
+
+import           Data.List
 import qualified Control.Monad.ST              as ST
+import qualified Data.Map                      as M
 import           Data.STRef
 
 class Ord v => Vector v where
@@ -34,9 +38,47 @@ data Settings e v = Settings { i :: Int -> [e] -> [v]
                              , threshold :: Double
                              }
 
+data KMeansSt s v = KMeansSt
+  { centroids :: STRef s [v]
+  , steps :: STRef s Int
+  }
+
+newCentroids :: (Vector v, Vectorizable e v) => M.Map v [e] -> [v]
+newCentroids m = (M.elems . fmap (\x -> (centroid . map toVector) x)) m
+
+clusterAssignments :: (Vector v, Vectorizable e v) => [v] -> [e] -> M.Map v [e]
+clusterAssignments centrs points =
+  let initialMap = M.fromList $ zip centrs (repeat [])
+  in  foldr
+        (\p m ->
+          let chosenC = minimumBy (compareDistance p) centrs
+          in  M.adjust (p :) chosenC m
+        )
+        initialMap
+        points
+ where
+  compareDistance p x y =
+    compare (distance x $ toVector p) (distance y $ toVector p)
 
 kMeans :: (Vector v, Vectorizable e v) => Settings e v -> [e] -> ([v], Int)
 kMeans (Settings i k threshold) points = kMeans' (i k points) points threshold
 
+-- todo clean this up w/ a record holding the state type
+
 kMeans' :: (Vector v, Vectorizable e v) => [v] -> [e] -> Double -> ([v], Int)
-kMeans' centroids points threshold = _todo
+kMeans' centroids points threshold = ST.runST $ do
+  steps       <- newSTRef 0
+  stCentroids <- newSTRef centroids
+  kMeans'' stCentroids steps
+  v1 <- readSTRef stCentroids
+  s1 <- readSTRef steps
+  return (v1, s1)
+ where
+  kMeans'' stC st = do
+    prevCentrs <- readSTRef stC
+    let assignments = clusterAssignments prevCentrs points
+        newCentrs   = newCentroids $ clusterAssignments prevCentrs points
+    writeSTRef stC newCentrs
+    modifySTRef' st (+ 1)
+    let err = sum $ zipWith distance prevCentrs newCentrs
+    if err < threshold then return () else kMeans'' stC st
