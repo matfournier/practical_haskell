596c95aa1670fe926e521920193775bc1f5f58e5
Mathew Fournier <mat@matfournier.com>
2019-10-24 22:53:03 -0700

    Ex9-1 w/ ReaderT IO

--- src/ch9/Ch9.hs
+++ src/ch9/Ch9.hs
@@ -2,6 +2,7 @@
 
 module Ch9 where
 import           Control.Monad.Loops
+import           Control.Monad.Reader
 import           System.Random
 import           Data.Char
 
@@ -66,3 +67,51 @@ randomDo = do
 
 
 -- exercise 9-1
+
+
+data GameConfig = GameConfig { _min :: Int
+                             , _max :: Int
+                             , _numTry :: Int
+                             } deriving (Eq, Ord, Show)
+
+
+-- this sucks
+gameLoop :: GameConfig -> Int -> Int -> IO [Char]
+gameLoop cfg@(GameConfig min max stop) target tries = do
+  if tries >= stop then doStop else doContinue cfg
+ where
+  doStop = do
+    return "Number of tries exceeded"
+  doContinue cfg = do
+    putStrLn "guess:"
+    guess <- fmap read getLine
+    if guess == target
+      then return "You win"
+      else gameLoop cfg target (tries + 1)
+
+
+-- better
+readerGameLoop :: Int -> Int -> ReaderT GameConfig IO ()
+readerGameLoop target n = do
+  (GameConfig min max stop) <- ask
+  if n >= stop
+    then lift
+      $ putStrLn ("too many guesses. The right answer was " ++ show target)
+    else do
+      lift $ putStrLn "guess again"
+      (guess :: Int) <- lift $ fmap read getLine
+      if guess == target
+        then lift $ putStrLn "you win"
+        else readerGameLoop target (n + 1)
+
+game :: IO ()
+game = do
+  let cfg = GameConfig { _min = 3, _max = 17, _numTry = 5 }
+  putStrLn
+    (  "Guessing game! guess between "
+    ++ (show $ _min cfg)
+    ++ " and "
+    ++ (show $ _max cfg)
+    )
+  target <- randomRIO (_min cfg, _max cfg)
+  runReaderT (readerGameLoop target 0) cfg
